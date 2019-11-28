f38ae8b88cae64fa539c17a1bc4394f8bf886ae2
Пользователь <maksim.durov45@gmail.com>
2019-11-28 18:07:22 +0200

    initial commit

---
+++ app/src/main/java/com/durov/recyclerviewkotlin_01/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.durov.recyclerviewkotlin_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val list: ArrayList<String> = ArrayList()
+    private lateinit var myItemViewAdapter: MyItemViewAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initList()
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        myItemViewAdapter = MyItemViewAdapter(list,this
+        ) { itemClick->Unit
+            Toast.makeText(this,itemClick,Toast.LENGTH_SHORT).show()}
+        recyclerView.adapter = myItemViewAdapter
+    }
+
+    private fun initList(){
+        for(i in 0..100){
+            list.add(i.toString())
+        }
+    }
+}
---
+++ app/src/main/java/com/durov/recyclerviewkotlin_01/MyItemViewAdapter.kt
@@ -0,0 +1,41 @@
+package com.durov.recyclerviewkotlin_01
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_view.view.*
+
+class MyItemViewAdapter(
+    private val items:ArrayList<String>,
+    private val context: Context,
+    private val itemClick: (String)-> Unit
+    )
+    :RecyclerView.Adapter<MyItemViewAdapter.ViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_view,parent,false),
+            itemClick)
+    }
+
+    override fun onBindViewHolder(holder : ViewHolder, position: Int) {
+       holder.bindModel(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ViewHolder(itemView: View,
+        val itemClick:(String)->Unit): RecyclerView.ViewHolder(itemView){
+        fun bindModel(model: String) {
+            with(model) {
+                itemView.setOnClickListener { itemClick(this) }
+                itemView.textView.text = model
+            }
+        }
+    }
+}
